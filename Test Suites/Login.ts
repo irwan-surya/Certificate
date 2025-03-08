cdf20a1b5d3e8bd4907c4511fb778d8728f5a0f8
irwan <irwansurya73@gmail.com>
2025-03-08 14:37:03 +0700

    login test suite

---
+++ Test Suites/Login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>ba8a3b96-58e0-4a88-9053-47209c15578b</testSuiteGuid>
+</TestSuiteEntity>
