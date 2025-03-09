ceb27fe3a3190240f03113748cbe1befd9b65424
irwan <irwansurya73@gmail.com>
2025-03-09 10:45:07 +0700

    commmit test suite login

--- Test Suites/Login.ts
+++ Test Suites/Login.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ba8a3b96-58e0-4a88-9053-47209c15578b</testSuiteGuid>
+   <testCaseLink>
+      <guid>9f681ff8-ad4b-40bf-a64d-0f0b7c450f1e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
